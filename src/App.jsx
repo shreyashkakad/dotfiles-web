5e376b28c79cce705f12f1b73ad9ccd6e5b73a11
shreyash0x3 <shreyashkakad03@gmail.com>
2025-07-20 22:06:15 +0530

    googleAnalytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import Contact from "./pages/Contact";
 import Upload from "./pages/Upload";
+import ReactGA from "react-ga4";
 
 function App() {
+
+    useEffect(() => {
+        ReactGA.initialize(import.meta.env.VITE_GAID); 
+        ReactGA.send("pageview");
+    }, []);
 
     return (
         <BrowserRouter>
